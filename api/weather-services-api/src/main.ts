ba25fd3888a951211c0197a99d1e05b5fc278c02
tylersteck <steckdev@live.com>
2024-10-31 22:15:17 -0600

    chore: Adjusting the package to use node v22. Also seeing if the ci will work properly with github. Also updating the helmet and cors for security to ensure best practices. Also updating the sample.env for reference.

--- api/weather-services-api/src/main.ts
+++ api/weather-services-api/src/main.ts
@@ -1,3 +1,5 @@
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -6,18 +8,28 @@ const APP_TITLE = 'Weather Service Api';
 const APP_DESCRIPTION =
   'Weather service api that provides weather information.';
 const APP_VERSION = '1.0';
+const WINDOW_MS = 15 * 60 * 1000;
+const MAX_REQUESTS = 1000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+
+  app.setGlobalPrefix('api/v1');
+
+  app.use(
+    rateLimit({
+      windowMs: WINDOW_MS,
+      max: MAX_REQUESTS,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle(APP_TITLE)
     .setDescription(APP_DESCRIPTION)
     .setVersion(APP_VERSION)
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'jwt',
-    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
